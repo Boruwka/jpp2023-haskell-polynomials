686be546a55b3603ec158f8b84ab9a69cb98e88c
Jagoda Bracha <jb429153@students.mimuw.edu.pl>
2023-03-24 19:31:16 +0100

    posprzatanie dense poly

--- DensePoly.hs
+++ DensePoly.hs
@@ -13,36 +13,41 @@ instance Polynomial DensePoly where
     x = (P [0, 1])
     evalP (P []) x = 0
     evalP (P (hd:tl)) x = hd + x * (evalP (P tl) x) -- warto by przerobić na ogonowe
-    shiftP 0 (P tab) = (P tab)
-    shiftP k (P tab) = shiftP (k-1) (P (0:tab))
+    shiftP n (P tab) = P (shift_array n tab)
     degree (P []) = -1
     degree (P (hd:tl)) = 1 + (degree (P tl))
     nullP (P []) = True
     nullP _ = False
 
-    
+shift_array :: (Num a) => Int -> [a] -> [a]
+shift_array 0 tab = tab
+shift_array n tab = shift_array (n-1) (0:tab)
+
+  
 
 instance (Eq a, Num a) => Num (DensePoly a) where
-    (+) (P []) p = p
-    (+) p (P []) = p
-    (+) (P (h1:t1)) (P (h2:t2)) = P ((h1 + h2):(unP ((+) (P t1) (P t2))))
-    (*) _ (P []) = (P [])
-    (*) (P []) _ = (P [])
-    (*) (P (h1:t1)) (P (h2:t2)) = ((+) ((+) (P [h1 + h2]) (P (multiply_helper 1 h1 t2))) ((+) (P (multiply_helper 1 h2 t1)) (P ((shift 2) (unP ((*) (P t1) (P t2)))))))
-        where 
-            shift 0 tab = tab
-            shift shifter tab = shift (shifter - 1) (0:tab)
-    (-) (P []) p = p
-    (-) p (P []) = p
-    (-) (P (h1:t1)) (P (h2:t2)) = P ((h1 - h2):(unP ((-) (P t1) (P t2))))
+    (+) (P tab1) (P tab2) = P (add_arrays tab1 tab2)
+    (*) (P tab1) (P tab2) = P (multiply_arrays tab1 tab2)
     negate (P []) = (P [])
     negate (P (h:t)) = P ((-h):(unP (negate (P t))))
-    abs (P []) = (P [])
-    abs (P (h:t)) = P ((abs h):(unP (abs (P t))))
-    -- jeszcze signum i fromInteger tylko nwm jak to ma wyglądać
+    abs = undefined
     signum p = undefined 
     fromInteger 0 = (P [])
     fromInteger k = (P [(fromInteger k)])
+    
+    
+add_arrays :: (Num a) => [a] -> [a] -> [a]
+add_arrays [] p = p
+add_arrays p [] = p
+add_arrays (h1:t1) (h2:t2) = (h1 + h2):(add_arrays t1 t2)
+
+multiply_arrays :: (Num a) => [a] -> [a] -> [a]
+multiply_arrays _ [] = []
+multiply_arrays [] _ = []
+multiply_arrays (h1:t1) (h2:t2) = (add_arrays (add_arrays ([h1 + h2]) (multiply_helper 1 h1 t2)) (add_arrays (multiply_helper 1 h2 t1) ((shift 2) (multiply_arrays t1 t2))))
+        where 
+            shift 0 tab = tab
+            shift shifter tab = shift (shifter - 1) (0:tab)
     
 multiply_helper :: (Num a) => Int -> a -> [a] -> [a]
 -- mnoży każdy element tab przez multiplier i przesuwa w prawo o shifter
